420740b766d3c231a8f72f84a37574ecdacbc1a6
Luan Chiodini <a.luanjc@gmail.com>
2024-08-30 18:55:38 -0300

    feat: Added file env and tsc config, and initial config folder build

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from "./server/Server";
 
-server.listen(3333, () => console.log("Servidor iniciou!"));
+server.listen(process.env.PORT || 3333, () => {
+  console.log(`App rodando na porta ${process.env.PORT || 3333}`);
+});
---
+++ src/server/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+  return res.send("Olá, DEV!");
+});
+
+router.post("/teste", (req, res) => {
+  console.log(req.body);
+
+  return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+});
+
+export { router };
