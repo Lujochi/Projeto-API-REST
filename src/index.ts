845209cdf7d5febf45843fa71d2125573232ffae
Luan Chiodini <a.luanjc@gmail.com>
2024-08-30 16:02:25 -0300

    feat: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 import { server } from "./server/Server";
 
-server.listen(3333, () => console.log("Servidor iniciou!"))
+server.listen(3333, () => console.log("Servidor iniciou!"));
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,11 +1,9 @@
-import express from "express"
+import express from "express";
 
 const server = express();
 
 server.get("/", (req, res) => {
+  return res.send("Olá, DEV!");
+});
 
-    return res.send("Olá, DEV!")
-})
-
-
-export { server }
+export { server };
