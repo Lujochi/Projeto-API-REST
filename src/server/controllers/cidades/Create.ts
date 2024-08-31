808d1748c2967eca9a97807f8f9308ae8219f91d
Luan Chiodini <a.luanjc@gmail.com>
2024-08-31 10:12:43 -0300

    feat: Add message errors and translate message errors

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -12,15 +12,19 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
-  } catch (error) {
-    const yupError = error as yup.ValidationError;
-
-    return res.json({
-      errors: {
-        default: yupError.message,
-      },
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
     });
+  } catch (err) {
+    const yupError = err as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
   }
 
   console.log(validatedData);
