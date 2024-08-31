1ed907186f079bcddd8e82dc5ffc402ae22ad6db
Luan Chiodini <a.luanjc@gmail.com>
2024-08-31 16:55:53 -0300

    feat: Created endpoints, getById, Update and delete

--- src/server/controllers/cidades/DeleteById.ts
+++ src/server/controllers/cidades/DeleteById.ts
@@ -4,20 +4,19 @@ import * as yup from "yup";
 
 import { validation } from "../../shared/middleware";
 
-interface ICidades {
-  nome: string;
+interface IParamProps {
+  id?: number;
 }
-
-export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidades>(
+export const deleteByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamProps>(
     yup.object().shape({
-      nome: yup.string().required().min(3),
+      id: yup.number().integer().required().moreThan(0),
     })
   ),
 }));
 
-export const create = async (req: Request<{}, {}, ICidades>, res: Response) => {
-  console.log(req.body);
+export const deleteById = async (req: Request<IParamProps>, res: Response) => {
+  console.log(req.params);
 
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -8,10 +8,30 @@ router.get("/", (req, res) => {
   return res.send("Olá, DEV!");
 });
 
+router.get(
+  "/cidades",
+  CidadesController.getAllValidation,
+  CidadesController.getAll
+);
+router.get(
+  "/cidades/:id",
+  CidadesController.getByIdValidation,
+  CidadesController.getById
+);
 router.post(
   "/cidades",
   CidadesController.createValidation,
   CidadesController.create
+);
+router.put(
+  "/cidades/:id",
+  CidadesController.updateByIdValidation,
+  CidadesController.updateById
+);
+router.delete(
+  "/cidades/:id",
+  CidadesController.deleteByIdValidation,
+  CidadesController.deleteById
 );
 
 export { router };
