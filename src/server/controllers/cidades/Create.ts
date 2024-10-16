295fadcb1808130713ebaa034c5fc631ed27b51c
Luan Chiodini <a.luanjc@gmail.com>
2024-10-16 15:59:28 -0300

    feat: Created model for cities

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,21 +3,21 @@ import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 
 import { validation } from "../../shared/middleware";
+import { ICidade } from "../../database/models";
 
-interface ICidades {
-  nome: string;
-}
+interface IBodyProps extends Omit<ICidade, "id"> {}
 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<ICidades>(
+  body: getSchema<IBodyProps>(
     yup.object().shape({
       nome: yup.string().required().min(3),
     })
   ),
 }));
 
-export const create = async (req: Request<{}, {}, ICidades>, res: Response) => {
-  console.log(req.body);
-
+export const create = async (
+  req: Request<{}, {}, IBodyProps>,
+  res: Response
+) => {
   return res.status(StatusCodes.CREATED).json(1);
 };
